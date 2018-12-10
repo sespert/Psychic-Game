5d42b2b77649fe6d9a738079b89d13c3e1030ee6
sespertg <sespert@gmail.com>
2018-12-10 14:56:14 -0500

    Final Submission

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,81 +19,63 @@ var lossesCount = document.getElementById("losses");
 var guessesCount = document.getElementById("guessesLeft");
 var userChoice = document.getElementById("guessLetter");
 
-
-
 // ==========================================================================
-
 // FUNCTIONS
 // ==============================================================================
 
-//Update number of wins
-function updateWins() {
+//Update the initial statistics
+function updateStats() {
     winCount.textContent = "Wins: " + wins;
-}
-//Update number of losses
-function updateLosses() {
     lossesCount.textContent = "Losses: " + losses;
-}
-//Update guesses left
-function updateGuess() {
     guessesCount.textContent = "Guesses Left: " + guesses;
-}
-//Update the guesses of the user
-function updateLettersChosen() {
     userChoice.textContent = "Your Guesses so far: " + letterGuess + " ";
 }
 
-//When called, this function generates new letter
+//When called, this function generates a new letter
 function updateRandomLetter() {
     computerLetter = ('abcdefghijklmnopqrstuvwxyz').split('')[(Math.floor(Math.random() * 26 ))];
 }
 
-// This is the function to listen to the user key pressed and .....
-
+//Game starts with computer picking a letter
 updateRandomLetter();
 
+// Then the user chooses a letter, and this function compares it to the computer one
 document.onkeyup = function(event) {
     var key = event.key.toLowerCase();
-    //var userWin = false;
+    
+    //The game keeps playing until number of guesses equals 0
     if (guesses > 0) {
+        
         if (key === computerLetter) {
-            wins++;  
-            updateWins();
-            updateLosses();         
-            guesses = 9; 
-            updateGuess();          
+            wins++; 
+            guesses = 9;      
             letterGuess = [];
-            updateLettersChosen();
+            updateStats();
             updateRandomLetter();
+            alert("Great job! The letter was indeed " + key + "!");
 
-            } else if (key !== computerLetter) {
-            updateWins();
-            updateLosses();      
-            guesses--;
-            updateGuess();          
-            letterGuess.push(key);
-            updateLettersChosen();
+        } else if (key !== computerLetter) { 
+            if (letterGuess.includes(key)) {
+                alert("You've already picked that letter, please choose another one");
+            } else {
+                guesses--;        
+                letterGuess.push(key);
+                updateStats();
+            }
         }
+
     } else if (guesses === 0) {
-        updateWins();
         losses++;
-        updateLosses();
         guesses = 9;
-        updateGuess();
         letterGuess = [];
-        updateLettersChosen();
+        alert("Sorry! No more guesses. The letter I chose is " + computerLetter);
+        updateStats();
         updateRandomLetter();
     }
-    console.log(computerLetter);
-    console.log(wins);
-    console.log(losses);
-    console.log(guesses);
-    console.log(key);
-    console.log(letterGuess);
 }
 
 
-  
+
 
 
 
